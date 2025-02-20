5a7816e49fa8b162988f015ca3854f759df3313a
John Doe <johndoe@example.com>
2025-02-20 18:27:23 +0100

    Lab functions-arrays done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,15 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers( a , b) {
+
+    if (a < b){
+        return a
+    }
+ 
+    else{
+        return b
+    }
+}
+console.log(10 , 6)
 
 
 
@@ -7,23 +17,79 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
 
+function findLongestWord(words) {
 
+    if(words.length === 0){
+
+        return null
+    }
+
+    let LongestWord = words[0];
+
+    for (let i = 0; i < words.length; i++){
+        
+          if (words[i] > LongestWord.length){
+
+             LongestWord = words[i]
+             }
+
+         return LongestWord
+    } }
+ console.log(findLongestWord(words))
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers){
+
+    let sum = 0;  
+    let i = 0;  
+    
+    
+    while (i < numbers.length) {
+      sum += numbers[i];  
+      i++;  
+    }
+    
+    return sum;  
+  }
+
+    //function sumNumbers(numbers){
+
+   // return numbers.reduce((accumulator, currentValue) => accumulator + currentValue)
+    
+
+    
 
 
 
 
 // Iteration 4 | Numbers Average
+
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+  
+  if (numbers2.length === 0) {
+    return 0;
+  }
+  
+
+  if (numbers2.length === 1) {
+    return numbers2[0];
+  }
+  
+  const sum = numbers2.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  
+  const average = sum / numbers2.length;
+  
+  return average;
+}
+
+
+  
 
 
 
